033b291a3d68fe9130c987b34d00408a9653260b
Manash Debnath <itsmanash1999@gmail.com>
2023-11-07 17:42:39 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+// import trash from '../trash.png'
+import DeleteIcon from '@mui/icons-material/Delete';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    const [state, setState] = useState([]);
+    const [perpage,setPerpage]=useState(10);
+    const [currentpage,setCurrentpage]=useState(1);
+
+    useEffect(()=>{
+      axios.get('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
+      .then(res=>setState(res.data))
+      .catch((e)=>{console.log('error',e)})  
+    })
+
+    const numberofTotalPages= Math.ceil(state.length/perpage);
+    const pages = [...Array(numberofTotalPages + 1).keys()].slice(1);
+    const pageEndingContent=currentpage*perpage;
+    const pageStartingContent=pageEndingContent-perpage;
+
+    const visibleContent=state.slice(pageStartingContent,pageEndingContent);
+
+    const prevPage=()=>{
+      if(currentpage !== 1){
+        setCurrentpage(currentpage-1)
+      }
+    }
+    const nextPage=()=>{
+      if(currentpage !== numberofTotalPages){
+        setCurrentpage(currentpage + 1)
+      }
+    }
+
+    return( 
+      <>
+        <table>
+                <tr>
+                  <th>{<input type="checkbox"/>}</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th>Action</th>
+                </tr>
+              
+        {visibleContent.map((data) => {
+            return ( 
+                <tr key={data.id}>
+                  <td>{<input type="checkbox"/>}</td>
+                  <td>{data.name}</td>
+                  <td>{data.email}</td>
+                  <td>{data.role}</td>
+                <td><span><BorderColorIcon/></span><span><DeleteIcon/></span></td>
+                </tr>  
+          )      
+        })} 
+        </table>
+        <button onClick={prevPage}>prev</button>
+        
+        {pages.map(res=>(<span key={res}
+         onClick={()=> setCurrentpage(res)}>{res}</span>))}
+        
+        <button onClick={nextPage}>next</button>
+      </>    
+    )
+    
+    
+      
+  }
+  
+    
+
 
 export default App;
---
+++ src/Content.jsx
@@ -0,0 +1,14 @@
+export default function Content(){
+    return(
+        <div>
+            <table>
+              <tr>
+                <th>id</th>
+                <th>name</th>
+                <th>email</th>
+                <th>role</th>
+              </tr>
+            </table>
+        </div>
+    )
+}
